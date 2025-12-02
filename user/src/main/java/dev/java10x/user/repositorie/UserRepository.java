1138b2c90da5ec82bcecee153d58c4df1b306e9f
Pontinn <leo.pontin2@gmail.com>
2025-12-02 10:25:20 -0300

    feat: remove default files

--- user/src/main/java/dev/java10x/user/repositorie/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.java10x.user.repositorie;
-import dev.java10x.user.domain.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<UserModel, UUID> {
-}
